36991f1f539a2366961b99e7a87f534f81685908
devid <nnick4673@gmail.com>
2022-10-02 18:50:54 -0300

    Rotas de navegação do front e a rota de erro foram implementadas

--- index.php
+++ index.php
@@ -3,7 +3,22 @@ ob_start();
 session_start();
 
 require __DIR__."/vendor/autoload.php";
+use CoffeeCode\Router\Router;
 
-echo "Olá Mundo!";
+$router = new Router(site());
+
+$router->group(NULL);
+$router->get("/", "Web:login", "web.login");
+$router->get("/cadastrar", "Web:register", "web.register");
+$router->get("/recuperar", "Web:forget", "web.forget");
+$router->get("/senha/{email}/{forget}", "Web:reset", "web:reset");
+
+$router->group("ops");
+$router->get("/{errcode}", "Web:error", "web.error");
+
+$router->dispatch();
+
+if ($router->error())
+	$router->redirect("web.error", ["errcode" => $router->error()]);
 
 ob_end_flush();
